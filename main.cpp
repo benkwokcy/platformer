aab1feeb0345b88e0ec3523e75ede8769a9b0494
Ben Kwok <ben.kwok.cy@gmail.com>
2021-04-05 00:26:11 -0700

    Clean up

--- main.cpp
+++ main.cpp
@@ -1,17 +1,32 @@
 #include <SDL2/SDL.h>
 #include <iostream>
+#include <stdexcept>
 
 using namespace std;
 
 int main()
 {
-	if (auto res = SDL_Init(SDL_INIT_VIDEO); res == -1) { assert(false); };
-	auto window = SDL_CreateWindow("GAME_NAME", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
-	assert(window != nullptr);
-	auto renderer = SDL_CreateRenderer(window, -1, 0);
+	// Create Window and Renderer
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+	char window_name[] = "Platformer";
+	int window_width = 800;
+	int window_height = 600;
 
+	if (auto res = SDL_Init(SDL_INIT_VIDEO); res == -1) {
+		throw runtime_error("Failed SDL_Init.");
+	}
+	if (window = SDL_CreateWindow(window_name, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, window_width, window_height, SDL_WINDOW_SHOWN); window == nullptr) {
+		throw runtime_error("Failed SDL_CreateWindow.");
+	}
+	if (renderer = SDL_CreateRenderer(window, -1, 0); renderer == nullptr) {
+		throw runtime_error("Failed SDL_CreateRenderer.");
+	}
+
+	// Main Loop
 	bool is_running = true;
 	SDL_Event event;
+
 	while (is_running) {
 		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT) {
@@ -24,6 +39,7 @@ int main()
 		SDL_Delay(16);
 	}
 
+	// Destroy Window and Renderer
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
