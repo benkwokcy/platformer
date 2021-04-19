c8614ebf31bb76b63dc1307f1ac61c8d25a355d8
Ben Kwok <ben.kwok.cy@gmail.com>
2021-04-18 22:40:14 -0700

    Use a vector of pigs instead of just one pig

--- code/scenes/Level.hpp
+++ code/scenes/Level.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <algorithm>
 #include <vector>
 
 #include "Scene.hpp"
@@ -14,38 +15,54 @@ public:
         level("assets/level.tmx")
     {
         player = create_player(level.markers.at("PlayerSpawn").x, level.markers.at("PlayerSpawn").y);
-        pig = create_pig(level.markers.at("PigSpawn").x, level.markers.at("PigSpawn").y);
+        pigs.push_back(create_pig(level.markers.at("PigSpawn").x, level.markers.at("PigSpawn").y));
     }
 
     ~Level() {
         delete player;
-        delete pig;
+        for (auto& pig : pigs) { 
+            delete pig; 
+        }
     }
 
     void paint() override {
         level.paint();
         player->paint();
-        pig->paint();
+        for (auto& pig : pigs) { 
+            pig->paint(); 
+        }
     }
 
     void handle_event(InputEvent e) override {
         player->handle_event(e);
-        pig->handle_event(e);
+        for (auto& pig : pigs) { 
+            pig->handle_event(e); 
+        }
     }
 
     void tick() override {
+        // update position
         player->tick();
-        pig->tick();
+        for (auto& pig : pigs) { 
+            pig->tick(); 
+        }
+        // collide with level
         for (auto& c : level.collisions) {
             player->collide_immovable(c);
-            pig->collide_immovable(c);
+            for (auto& pig : pigs) { 
+                pig->collide_immovable(c); 
+            }
         }
-        player->collide_movable(*pig);
+        // collide with objects
+        for (auto& pig : pigs) { 
+            player->collide_movable(*pig); 
+        }
+        // update camera
         Camera::tick(static_cast<int>(player->x));
     }
 
 private:
     Tilemap level;
     Entity* player;
-    Entity* pig;
+    std::vector<Entity*> pigs;
 };
