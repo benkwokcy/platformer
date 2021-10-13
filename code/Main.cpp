4e8bc63a98fba0b27242a5ca82f3c42a8792ca56
Ben Kwok <ben.kwok.cy@gmail.com>
2021-10-13 12:52:18 -0500

    Move PerformanceMonitor into its own file

--- code/Main.cpp
+++ code/Main.cpp
@@ -1,34 +1,8 @@
 #include <SDL2/SDL.h>
 
-#include <iostream>
-
 #include "Window.hpp"
 #include "Game.hpp"
-
-using namespace std;
-
-class PerformanceMonitor {
-public:
-    int total_time_in_frame = 0;
-    int max_time_in_frame = 0;
-    int num_frames = 0;
-    const char* A = "Average time per frame: ";
-    const char* B = " ms | Maximum time per frame: ";
-    const char* C = " ms\n";
-
-    void update(int time_in_frame) {
-        max_time_in_frame = std::max(max_time_in_frame, time_in_frame);
-        total_time_in_frame += time_in_frame;
-        num_frames++;
-
-        if (num_frames == 60) {
-            std::cout << A << total_time_in_frame / num_frames << B << max_time_in_frame << C;
-            num_frames = 0;
-            total_time_in_frame = 0;
-            max_time_in_frame = 0;
-        }
-    }
-};
+#include "util/PerformanceMonitor.hpp"
 
 int main() {
     Window::start();
---
+++ code/util/PerformanceMonitor.hpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class PerformanceMonitor {
+public:
+    int total_time_in_frame = 0;
+    int max_time_in_frame = 0;
+    int num_frames = 0;
+    const char* A = "Average time per frame: ";
+    const char* B = " ms | Maximum time per frame: ";
+    const char* C = " ms\n";
+
+    void update(int time_in_frame) {
+        max_time_in_frame = std::max(max_time_in_frame, time_in_frame);
+        total_time_in_frame += time_in_frame;
+        num_frames++;
+
+        if (num_frames == 60) {
+            std::cout << A << total_time_in_frame / num_frames << B << max_time_in_frame << C;
+            num_frames = 0;
+            total_time_in_frame = 0;
+            max_time_in_frame = 0;
+        }
+    }
+};
